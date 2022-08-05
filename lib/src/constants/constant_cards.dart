4a61a3a6517caded46a1d79a29e54a0819316ace
Arthur Pointel <arthurpointel@outlook.com>
2022-08-05 23:01:17 +0200

    Adding provider logic

---
+++ lib/src/constants/constant_cards.dart
@@ -0,0 +1,16 @@
+import 'package:jeu_du_roi/src/models/GameCard.dart';
+
+GameCard CARD_ROI_DES_MELANGES = GameCard(
+    cardId: 1,
+    title: 'LE ROI DES MELANGES',
+    text: "Le dernier 'Roi des mélanges' est tombé. Bois le calice !"
+);
+
+GameCard END_GAME = GameCard(
+    cardId: 0,
+    title: 'FIN DE LA PARTIE',
+    text: ""
+);
+
+
+
---
+++ lib/src/constants/constants.dart
@@ -0,0 +1,16 @@
+//App Title
+const String  APP_TITLE  = "Napusoif";
+
+//Page titles
+const String  PAGETITLE_PLAYERSELECTION  = "KIVEUJOUER";
+
+//Inputs
+const String STRING_PLAYERDIV = "Player";
+const String STRING_PLAYERBUTTON = "CÉPARTI !";
+
+//Card files
+const String STRING_BASEMODEFILEPATH = "assets/datas/cards_new_base_version.json";
+const String STRING_HEAVYMODEFILEPATH = "assets/datas/cards_original_version.json";
+
+//Maximum player
+const int MAX_PLAYER = 12;
